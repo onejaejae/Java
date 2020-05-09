1e953158c17ac8b1f13ad721028b55d9d8778291
Cho Won Jae <yoteamo7@naver.com>
2020-05-09 17:25:33 +0900

    first commmit

---
+++ src/com/codechobo/book/PackageTest.java
@@ -0,0 +1,31 @@
+package com.codechobo.book;
+class calculator{
+	private int left,right;
+	
+	public void setOprands(int left,int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	private int sum() {
+		return this.left + this.right;
+	}
+	
+	public void sumDecoPlus() {
+		System.out.println("+++++" + sum() + "+++++");
+	}
+	public void sumDecoMinus() {
+		System.out.println("-----" + sum() + "-----");
+	}
+	
+}
+public class PackageTest {
+
+	public static void main(String[] args) {
+		calculator c = new calculator();
+		c.setOprands(10,20);
+		c.sumDecoMinus();
+		c.sumDecoPlus();
+	}
+
+}
